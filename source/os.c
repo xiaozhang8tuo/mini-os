b4f702707524889ab13cea1851cf4fee0ae5bac7
zyx <874279750@qq.com>
2023-08-03 06:19:00 -0700

    开启定时器中断, 中断控制器8259a

--- source/os.c
+++ source/os.c
@@ -21,6 +21,10 @@ uint32_t pg_dir[1024] __attribute__((aligned(4096))) = {
 };
 
 
+// idt表中的中断处理函数
+struct {uint16_t offset_l, selector, attr, offset_h;} idt_table[256] __attribute__((aligned(8))) = {1};
+
+
 // GDT表中的数据段和代码段设置
 struct { uint16_t limit_l, base_l, basehl_attr, base_limit;} gdt_table[256] __attribute__((aligned(8))) = {
     // 0x00cf9a000000ffff - 从0地址开始，P存在，DPL=0，Type=非系统段，32位代码段（非一致代码段），界限4G，
@@ -29,9 +33,47 @@ struct { uint16_t limit_l, base_l, basehl_attr, base_limit;} gdt_table[256] __at
     [KERNEL_DATA_SEG/8] = {0xffff, 0x0000, 0x9200, 0x00cf},
 };
 
-// 页目录表的第一项做恒等映射，不对应二级页表
-// 页目录表的第512项指向对应的二级页表，二级页表对应map_phy_buffer的地址
+
+// 反汇编结果
+// __asm__ __volatile__("outb %[v], %[p]"::[p]"d"(port), [v]"a"(data));
+// 7e54:	0f b7 55 f8          	movzwl -0x8(%ebp),%edx
+// 7e58:	0f b6 45 fc          	movzbl -0x4(%ebp),%eax
+// 7e5c:	ee                   	out    %al,(%dx)
+void outb(uint8_t data, uint16_t port)
+{
+    __asm__ __volatile__("outb %[v], %[p]"::[p]"d"(port), [v]"a"(data));
+}
+
+
+void timer_int (void);
+
 void os_init (void) {
+    // 初始化8259中断控制器，打开定时器中断
+    outb(0x11, 0x20);  // 开始初始化主芯片       
+    outb(0x11, 0xA0);  // 初始化从芯片   
+    outb(0x20, 0x21);  // 写ICW2，告诉主芯片中断向量从0x20开始   
+    outb(0x28, 0xA1);  // 写ICW2，告诉从芯片中断向量从0x28开始   
+    outb(1<<2, 0x21);  // 写ICW3，告诉主芯片IRQ2上连接有从芯片   
+    outb(2, 0xa1);     // 写ICW3，告诉从芯片连接g到主芯片的IRQ2上
+    outb(0x1, 0x21);   // 写ICW4，告诉主芯片8086、普通EOI、非缓冲模式   
+    outb(0x1, 0xA1);   // 写ICW4，告诉主芯片8086、普通EOI、非缓冲模式   
+    outb(0xfe, 0x21);  // 开定时中断，其它屏幕   
+    outb(0xff, 0xA1);  // 屏蔽所有中断   
+
+    // 设置定时器，每100ms中断一次
+    int tmo = (1193180);      // 时钟频率为1193180
+    outb(0x36, 0x43);               // 二进制计数、模式3、通道0
+    outb((uint8_t)tmo, 0x40);
+    outb(tmo >> 8, 0x40);
+
+    // 添加中断，设置第0x20号中断
+    idt_table[0x20].offset_h = (uint32_t)timer_int >> 16;
+    idt_table[0x20].offset_l = (uint32_t)timer_int & 0xffff;
+    idt_table[0x20].selector = KERNEL_CODE_SEG;
+    idt_table[0x20].attr = 0x8E00;      // 存在，DPL=0, 中断门
+
+    // 页目录表的第一项做恒等映射，不对应二级页表
+    // 页目录表的第512项指向对应的二级页表，二级页表对应map_phy_buffer的地址
     // 虚拟内存
     // 0x80000000开始的4MB区域的映射
     pg_dir[MAP_ADDR >> 22] = (uint32_t)pg_table | PDE_P | PDE_W | PDE_U;
