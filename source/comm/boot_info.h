a7001938e2f7d8a4d3e328c1ac5e32d1b5eee02a
zyx <874279750@qq.com>
2023-08-14 06:20:24 -0700

    feat: 读取内存信息

---
+++ source/comm/boot_info.h
@@ -0,0 +1,17 @@
+#ifndef BOOT_INFO_H
+#define BOOT_INFO_H
+#include "types.h"
+
+#define BOOT_RAM_REGION_MAX 10 
+
+typedef struct _boot_info_t {
+    // RAM区信息
+    struct {
+        uint32_t start;
+        uint32_t size;
+    }ram_region_cfg[BOOT_RAM_REGION_MAX];
+    int ram_region_count;
+}boot_info_t;
+
+
+#endif
--- source/loader/loader.h
+++ source/loader/loader.h
@@ -1,5 +1,18 @@
 #ifndef LOADER_H
 #define LOADER_H
 
+#include "comm/boot_info.h"
+#include "comm/types.h"
+
+// 内存检测信息结构
+typedef struct SMAP_entry {
+    uint32_t BaseL; // base address uint64_t
+    uint32_t BaseH;
+    uint32_t LengthL; // length uint64_t
+    uint32_t LengthH;
+    uint32_t Type; // entry Type
+    uint32_t ACPI; // extended
+}__attribute__((packed)) SMAP_entry_t;
+
 
 #endif
