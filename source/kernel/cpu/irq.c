1efadac7f3192411cbc697a68b9eab78f13b5240
zyx <874279750@qq.com>
2024-05-30 08:29:39 -0700

    feat(ASM:macro): 汇编中加入宏指令, 设置除零异常对应的中断处理函数

--- source/kernel/cpu/irq.c
+++ source/kernel/cpu/irq.c
@@ -5,6 +5,7 @@
 #define IDT_TABLE_NR 128
 
 void exception_handler_unknow(void);
+void exception_handler_division(void);
 
 static void do_default_handler (const char* message) {
     for (;;) {}
@@ -12,6 +13,11 @@ static void do_default_handler (const char* message) {
 
 void do_handler_unknow (_exception_frame_t* frame) {
     do_default_handler("unknow exception");
+}
+
+
+void do_handler_division (_exception_frame_t* frame) {
+    do_default_handler("division exception");
 }
 
 static gate_desc_t idt_table[IDT_TABLE_NR];
@@ -23,5 +29,17 @@ void irq_init(void) {
         GATE_P_PRESENT | GATE_DPL0 | GATE_TYPE_IDT);
     }
 
+    irq_install(IRQ0_DE, (irq_handler_t)exception_handler_division);
+
     lidt((uint32_t)idt_table, sizeof(idt_table));
+}
+
+void irq_install(int irq_num, irq_handler_t handler) {
+    if (irq_num >= IDT_TABLE_NR) {
+        return -1;
+    }
+
+    gate_desc_set(idt_table+irq_num, KERNEL_SELECTOR_CS, (uint32_t)handler, 
+        GATE_P_PRESENT | GATE_DPL0 | GATE_TYPE_IDT);
+    return 0;
 }
--- source/kernel/include/cpu/irq.h
+++ source/kernel/include/cpu/irq.h
@@ -2,15 +2,19 @@
 #define IRQ_H
 # include "comm/types.h"
 
+// 中断号码
+#define IRQ0_DE             0 //除0中断
 
 typedef struct _exception_frame_t {
     uint32_t gs, fs, es, ds;
     uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
+    uint32_t num, error_code;
     uint32_t eip, cs, eflags;
 } _exception_frame_t;
 
-
+typedef void (*irq_handler_t) (void);
 
 void irq_init(void);
+void irq_install(int num, irq_handler_t handler);
 
 #endif 
