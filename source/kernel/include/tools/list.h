59d51ecefe5d50d36b265cfee4807e144386a2b2
zyx <874279750@qq.com>
2024-06-26 09:45:46 -0700

    feat(list): Add insert delete api

--- source/kernel/include/tools/list.h
+++ source/kernel/include/tools/list.h
@@ -45,5 +45,11 @@ static inline list_node_t* list_last(list_t *list) {
     return list->last;
 }
 
+void list_insert_first(list_t *list, list_node_t *node);
+void list_insert_last(list_t *list, list_node_t *node);
+
+list_node_t* list_remove_first(list_t *list);
+list_node_t* list_remove(list_t *list, list_node_t *node);
+
 
 #endif
