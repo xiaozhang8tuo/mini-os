a9c0ca8f06ab1d696a9a11accbcac7a81bc71556
zyx <874279750@qq.com>
2024-07-23 18:28:19 -0700

    feat(bitmap): 添加bitmap相关的操作

--- source/kernel/include/tools/bitmap.h
+++ source/kernel/include/tools/bitmap.h
@@ -13,5 +13,10 @@ typedef struct _bitmap_g {
 
 void bitmap_init (bitmap_t * bitmap, uint8_t * bits, int count, int init_bit);
 int bitmap_byte_count (int bit_count);
+int bitmap_get_bit(bitmap_t* bitmap, int index);
+void bitmap_set_bit(bitmap_t* bitmap, int index, int count, int bit);
+int bitmap_is_set(bitmap_t* bitmap, int index);
+// 连续分配若干指定比特位，返回起始索引
+int bitmap_alloc_nbits(bitmap_t* bitmap, int bit, int count);
 
 #endif // BITMAP_H
--- source/kernel/tools/bitmap.c
+++ source/kernel/tools/bitmap.c
@@ -12,3 +12,57 @@ void bitmap_init (bitmap_t * bitmap, uint8_t * bits, int count, int init_bit) {
     int bytes = bitmap_byte_count(bitmap->bit_count);
     kernel_memset(bitmap->bits, init_bit ? 0xFF: 0, bytes);
 }
+
+int bitmap_get_bit(bitmap_t* bitmap, int index) {
+    // 从低到高 17 / 8 = 2
+    return bitmap->bits[index/8] & (1 << (index % 8));
+}
+
+void bitmap_set_bit(bitmap_t* bitmap, int index, int count, int bit){
+    for (int i = 0; (i < count) && (index < bitmap->bit_count); i++, index++) {
+        if (bit) {
+            bitmap->bits[index/8] |= 1 << (index % 8);
+        } else {
+            bitmap->bits[index/8] &= ~(1 << (index % 8));
+        }
+    }
+}
+
+int bitmap_is_set(bitmap_t* bitmap, int index) {
+    return bitmap_get_bit(bitmap, index) ? 1 : 0;
+}
+
+int bitmap_alloc_nbits (bitmap_t * bitmap, int bit, int count) {
+    int search_idx = 0;
+    int ok_idx = -1;
+
+    while (search_idx < bitmap->bit_count) {
+        // 定位到第一个相同的索引处
+        if (bitmap_get_bit(bitmap, search_idx) != bit) {
+            // 不同，继续寻找起始的bit
+            search_idx++;
+            continue;
+        }
+
+        // 记录起始索引
+        ok_idx = search_idx;
+
+        // 继续计算下一部分
+        int i;
+        for (i = 1; (i < count) && (search_idx < bitmap->bit_count); i++) {
+            if (bitmap_get_bit(bitmap, search_idx++) != bit) {
+                // 不足count个，退出，重新进行最外层的比较
+                ok_idx = -1;
+                break;
+            }
+        }
+
+        // 找到，设置各位，然后退出
+        if (i >= count) {
+            bitmap_set_bit(bitmap, ok_idx, count, ~bit);
+            return ok_idx;
+        }
+    }
+
+    return -1;
+}
