a8d7759f2180b20744f54ed04df0063f34b672c7
zyx <874279750@qq.com>
2025-03-07 21:10:37 -0800

    feat(disk): 识别系统中已有的磁盘

--- source/comm/cpu_instr.h
+++ source/comm/cpu_instr.h
@@ -26,6 +26,10 @@ static inline void outb(uint16_t port, uint8_t data) {
                         : [p]"d" (port), [v]"a" (data));
 }
 
+static inline void outw(uint16_t port, uint16_t data) {
+	__asm__ __volatile__("out %[v], %[p]" : : [p]"d" (port), [v]"a" (data));
+}
+
 static inline void sti(void)
 {
     __asm__ __volatile__("sti");
